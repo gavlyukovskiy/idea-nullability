6009aea144b09f2215fc64807b3965ce7f7c2343
Artur Havliukovskyi <agavlyukovskiy@gmail.com>
2024-04-16 12:41:45 +0200

    Add jupiter engine

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 dependencies {
 	implementation("org.jspecify:jspecify:0.3.0")
 	testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.2")
+	testImplementation("org.junit.jupiter:junit-jupiter-engine:5.10.2")
 	testImplementation("org.assertj:assertj-core:3.25.3")
 }
 
